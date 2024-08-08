b7d7e1511cdcc0ecc7dae38ef3d4d78529a1a7cb
mertemr <48881002+mertemr@users.noreply.github.com>
2024-08-08 12:41:12 +0300

    chore: Update query parameter validation in PredictParams model

--- api/src/models.py
+++ api/src/models.py
@@ -1,10 +1,13 @@
 from fastapi import Query
 from pydantic import BaseModel
 
+from src.config import MAX_CHARACTER_LENGTH
+
 
 class PredictParams(BaseModel):
     query: str = Query(
         ...,
         title="Query",
-        description="The query to predict"
+        description="The query to predict",
+        max_length=MAX_CHARACTER_LENGTH
     )

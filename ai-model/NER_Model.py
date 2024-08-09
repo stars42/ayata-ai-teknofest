6563f1a363b9887e3467d2c8bad848d32749ba15
mertemr <48881002+mertemr@users.noreply.github.com>
2024-08-09 13:45:49 +0300

    feat: Add NER_Model.py for data masking and aspect tagging

--- ai-model/NER_Model.py
+++ ai-model/NER_Model.py
@@ -1,53 +1,48 @@
-import re
-import unicodedata
-
+from numpy import argmax, ndarray
 import pandas as pd
-
-INPUT_DATA_FILE = "data_combined.csv"
-OUTPUT_CSV_FILE = "data_masked.csv"
-
-normalize = lambda text: unicodedata.normalize("NFKD", text) \
-                                    .encode("ascii", errors="ignore") \
-                                    .decode("utf-8")
+import torch
+from datasets import Dataset, load_metric
+from transformers import (AutoModelForTokenClassification, AutoTokenizer,
+                          Trainer, TrainingArguments)
 
 
-def load_data(csv_file: str):
-    data = pd.read_csv(csv_file)
-    texts = data["text"]
-    aspects = data["aspect"]
+INPUT_DATA_FILE = "train.txt"  # Veri setinin olduğu dosya
+LABELS_FILE = "labels.txt"  # Etiketlenmiş verilerin olduğu dosya
 
-    for index, item in enumerate(zip(texts, aspects)):
-        text, aspect = item
-        if normalize(text) != text or normalize(aspect) != aspect:
-            data.drop(index, inplace=True)
-
-    return data, texts, aspects
+tokenizer = AutoTokenizer.from_pretrained("dbmdz/bert-base-turkish-128k-uncased")
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+model = AutoModelForTokenClassification.from_pretrained(
+    "dbmdz/bert-base-turkish-128k-uncased", num_labels=len(tag2id)
+).to(device)
+metric = load_metric("seqeval") 
 
 
-def create_tags(texts: list[str], aspects: list[str]) -> pd.DataFrame:
-    """
-    Bu fonksiyon verilen text ve aspect'lerden oluşan listeleri alır ve aspect'lerin içinde geçtiği kelimeleri bulur.
-    """
-    labels = []
+def load_and_label(file_path: str):
+    with open(file_path, "r", encoding="utf-8") as file:
+        lines = file.readlines()
+
     sentences = []
+    labels = []
+    current_sentence = []
+    current_labels = []
 
-    for index, sentence in enumerate(texts):
-        words = sentence.split()
-        label = []
-        wrong_aspect = []
-        for word in words:
-            # * Burada aspect'lerin içinde geçtiği kelimeleri buluyoruz.
-            if re.search(re.escape(aspects[index]), word, flags=re.IGNORECASE):
-                label.append("1")
-            else:
-                label.append("0")
-        if check_label(label):
-            labels.append(label)
-            sentences.append(sentence)
+    for line in lines:
+        if line := line.strip():
+            word, label = line.split()
+            current_sentence.append(word)
+            current_labels.append(label)
         else:
-            wrong_aspect.append(aspects[index])
+            if current_sentence:
+                sentences.append(current_sentence)
+                labels.append(current_labels)
+                current_sentence = []
+                current_labels = []
+        
+    if current_sentence:
+        sentences.append(current_sentence)
+        labels.append(current_labels)
 
-    return pd.DataFrame({"Sentence": sentences, "Label": labels})
+    return sentences, labels
 
 
 def mask_aspects(text: str, aspects: list[str]) -> str:
@@ -60,17 +55,94 @@ def check_label(labels: list[str]) -> bool:
     return any(label == "1" for label in labels)
 
 
-def main(*args):
-    data, texts, aspects = load_data(INPUT_DATA_FILE)
+def get_labels(file_name: str):
+    with open(file_name, "r", encoding="utf-8") as file:
+        return [i.rstrip() for i in file.readlines()]
 
-    data["aspect"] = data["aspect"].apply(eval)
-    data["text"] = data.apply(lambda row: mask_aspects(row["text"], row["aspect"]), axis=1)
 
-    data.to_csv(OUTPUT_CSV_FILE, index=False)
+def encode_tags(tags: list[str], tag2id: dict[str, int]) -> list[int]:
+    return [tag2id[tag] for tag in tags]
 
-    # TODO devamı Google Colab'da yazılacak
+
+def tokenize_and_align_labels(examples: dict):
+    tokenized_inputs = tokenizer(
+        examples["sentences"], padding="max_length", max_length=512, truncation=True, is_split_into_words=True
+    )
     labels = []
-    result_df = create_tags(texts, aspects)
+    for i, label in enumerate(examples["labels"]):
+        word_ids = tokenized_inputs.word_ids(batch_index=i)
+        label_ids = []
+        for word_idx in word_ids:
+            if word_idx is None:
+                label_ids.append(-100)
+            else:
+                label_ids.append(label[word_idx])
+        label_ids += [-100] * (512 - len(label_ids))
+        labels.append(label_ids)
+
+    tokenized_inputs["labels"] = labels
+    return tokenized_inputs
+
+
+def compute_metrics(p: tuple[ndarray, ndarray]):
+    predictions, labels = p
+    predictions = argmax(predictions, axis=2)
+
+    true_predictions = [
+        [id2tag[p] for (p, l) in zip(prediction, label) if l != -100] for prediction, label in zip(predictions, labels)
+    ]
+    true_labels = [
+        [id2tag[l] for (p, l) in zip(prediction, label) if l != -100] for prediction, label in zip(predictions, labels)
+    ]
+
+    results = metric.compute(predictions=true_predictions, references=true_labels)
+    return {
+        "precision": results["overall_precision"],
+        "recall": results["overall_recall"],
+        "f1": results["overall_f1"],
+        "accuracy": results["overall_accuracy"],
+    }
+
+
+def main(*args):
+    global tag2id, id2tag
+
+    sentences, labels = load_and_label(INPUT_DATA_FILE)
+    labels = get_labels(LABELS_FILE)
+
+    data = pd.DataFrame({"sentences": sentences, "labels": labels})
+
+    tag2id = {tag: id for id, tag in enumerate(labels)}
+    id2tag = {id: tag for id, tag in enumerate(labels)}
+
+    data["labels"] = data["labels"].apply(lambda x: encode_tags(x, tag2id))
+    train_data = Dataset.from_pandas(data)
+
+    tokenized_dataset = train_data.map(tokenize_and_align_labels, batched=True)
+
+    train_test_split = tokenized_dataset.train_test_split(test_size=0.2)
+    tokenized_train_dataset = train_test_split["train"]
+    tokenized_test_dataset = train_test_split["test"]
+
+    training_args = TrainingArguments(
+        output_dir="./results",
+        evaluation_strategy="epoch",
+        learning_rate=2e-5,
+        per_device_train_batch_size=16,
+        per_device_eval_batch_size=16,
+        num_train_epochs=3,
+        weight_decay=0.01,
+    )
+
+    trainer = Trainer(
+        model=model,
+        args=training_args,
+        train_dataset=tokenized_train_dataset,
+        tokenizer=tokenizer,
+        eval_dataset=tokenized_test_dataset,
+        compute_metrics=compute_metrics,
+    )
+    trainer.train()
 
 
 if __name__ == "__main__":

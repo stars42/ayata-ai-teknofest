7eb0edac2fde2350566e33e8b2423dbffe5228a9
mertemr <48881002+mertemr@users.noreply.github.com>
2024-08-09 10:41:05 +0300

    chore: Update API endpoint and implement AI response handling in FastAPI file

--- api/main.py
+++ api/main.py
@@ -1,30 +1,49 @@
-from fastapi import APIRouter, FastAPI
+import logging
+
+import httpx
+from fastapi import Depends, FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import ORJSONResponse
 
 from src.models import PredictParams
+from src.config import AI_SERVER_HOST
+
 
 app = FastAPI(
-    title="AyataAI - API",
+    title="Teknofest - AyataAI API",
     docs_url="/",
 )
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["*"],
     allow_credentials=True,
-    allow_methods=["POST", "OPTIONS"],
+    allow_methods=["*"],
     allow_headers=["*"],
 )
 
-router = APIRouter(prefix="/api", tags=["API"])
+logger = logging.getLogger(__name__)
 
+logging.basicConfig(
+    level=logging.INFO,
+    format="[%(asctime)s] %(message)s",
+    handlers=[
+        logging.FileHandler("logs.log", encoding="utf-8"),
+        logging.StreamHandler()
+    ]
+)
 
-@router.post("/predict")
+@app.post("/predict")
 def predict(params: PredictParams = Depends()):
-    # TODO - Buraya AI ile ilgili işlemler gelecek
-    return ORJSONResponse(content={"query": params.query})
+    data: dict = httpx.get(AI_SERVER_HOST, params={"query": params.text}, timeout=30).json()
+    print(data)
+    entities: list = data["response"]["entity_list"]
+    for i in data["response"]["results"]:
+        entity: dict = i["entity"]
+        if not entity in entities:
+            entities.append(entity)
 
-app.include_router(router)
+    data["response"]["entity_list"] = entities
 
-__all__ = ["app"]
+    logger.info(f"Request: {params.text} | Response: {data['response']}")
+    return ORJSONResponse(content=data["response"]) 
